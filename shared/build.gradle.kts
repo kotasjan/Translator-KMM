d59e9a3841c2ca7ef17de54322be672875a544e2
Jan Kotas <kotasjn@gmail.com>
2023-09-23 23:33:59 +0200

    ⬆️ Update and fix dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,19 +2,13 @@ plugins {
     kotlin("multiplatform")
     kotlin("native.cocoapods")
     id("com.android.library")
+    kotlin("plugin.serialization") version libs.versions.kotlin
+    id("app.cash.sqldelight")
 }
 
-@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
 kotlin {
-    targetHierarchy.default()
-
-    androidTarget {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "1.8"
-            }
-        }
-    }
+    jvmToolchain(17)
+    androidTarget()
     iosX64()
     iosArm64()
     iosSimulatorArm64()
@@ -33,20 +27,57 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                // put your multiplatform dependencies here
+                implementation(libs.ktor.core)
+                implementation(libs.ktor.serialization)
+                implementation(libs.ktor.serialization.json)
+                implementation(libs.sqlDelight.runtime)
+                implementation(libs.sqlDelight.coroutines)
+                implementation(libs.kotlin.datetime)
             }
         }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
+                implementation(libs.assertk)
+                implementation(libs.turbine)
             }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation(libs.ktor.android)
+                implementation(libs.sqlDelight.android.driver)
+            }
+        }
+        val androidUnitTest by getting
+
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                implementation(libs.ktor.ios)
+                implementation(libs.sqlDelight.native.driver)
+            }
+        }
+        val iosX64Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
+        val iosTest by creating {
+            dependsOn(commonTest)
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
         }
     }
 }
 
 android {
     namespace = "cz.jankotas.translator"
-    compileSdk = 33
+    compileSdk = 34
     defaultConfig {
         minSdk = 24
     }
