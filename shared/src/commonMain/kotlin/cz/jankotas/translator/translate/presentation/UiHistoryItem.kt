9f3f933b9178585cf4e2fa521efb5f74937b38de
Jan Kotas <kotasjn@gmail.com>
2023-09-28 22:29:59 +0200

    ✨ Add ui class for history item

---
+++ shared/src/commonMain/kotlin/cz/jankotas/translator/translate/presentation/UiHistoryItem.kt
@@ -0,0 +1,11 @@
+package cz.jankotas.translator.translate.presentation
+
+import cz.jankotas.translator.core.presentation.UiLanguage
+
+data class UiHistoryItem(
+    val id: Long,
+    val fromText: String,
+    val toText: String,
+    val fromLanguage: UiLanguage,
+    val toLanguage: UiLanguage,
+)
