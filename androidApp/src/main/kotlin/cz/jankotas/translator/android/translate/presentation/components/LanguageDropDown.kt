3e02032de7e006d22c22b29b3ce7c5e927ae6b7e
Jan Kotas <kotasjn@gmail.com>
2023-10-06 13:50:46 +0200

    💄 Add language dropdown component

---
+++ androidApp/src/main/kotlin/cz/jankotas/translator/android/translate/presentation/components/LanguageDropDown.kt
@@ -0,0 +1,78 @@
+package cz.jankotas.translator.android.translate.presentation.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import cz.jankotas.translator.android.R
+import cz.jankotas.translator.android.core.theme.LightBlue
+import cz.jankotas.translator.core.presentation.UiLanguage
+
+@Composable
+fun LanguageDropDown(
+    language: UiLanguage,
+    isOpen: Boolean,
+    onClick: () -> Unit,
+    onDismiss: () -> Unit,
+    onSelectLanguage: (UiLanguage) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        DropdownMenu(expanded = isOpen, onDismissRequest = onDismiss) {
+            UiLanguage.allLanguages.forEach { langugage ->
+                LanguageDropDownItem(
+                    language = language,
+                    onClick = {
+                        onSelectLanguage(language)
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                )
+            }
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .clickable(onClick = onClick)
+                .padding(16.dp),
+        ) {
+            AsyncImage(
+                model = language.drawableRes,
+                contentDescription = language.language.langName,
+                modifier = Modifier.size(30.dp),
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
+                text = language.language.langName,
+                color = LightBlue,
+            )
+            Icon(
+                imageVector = if (isOpen) {
+                    Icons.Default.ArrowDropUp
+                } else {
+                    Icons.Default.ArrowDropDown
+                },
+                contentDescription = if (isOpen) {
+                    stringResource(id = R.string.close)
+                } else {
+                    stringResource(id = R.string.open)
+                },
+                tint = LightBlue,
+                modifier = Modifier.size(30.dp),
+            )
+        }
+    }
+}
---
+++ androidApp/src/main/kotlin/cz/jankotas/translator/android/translate/presentation/components/LanguageDropDownItem.kt
@@ -0,0 +1,32 @@
+package cz.jankotas.translator.android.translate.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import cz.jankotas.translator.core.presentation.UiLanguage
+
+@Composable
+fun LanguageDropDownItem(
+    language: UiLanguage,
+    onClick: () -> Unit,
+    modifier: Modifier,
+) {
+    DropdownMenuItem(onClick = onClick, modifier = modifier) {
+        Image(
+            painter = painterResource(id = language.drawableRes),
+            contentDescription = language.language.name,
+            modifier = Modifier.size(40.dp),
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = language.language.langName,
+        )
+    }
+}
