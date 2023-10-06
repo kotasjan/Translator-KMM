84015ff55f9457c8e8c7e89c36b413eb39261882
Jan Kotas <kotasjn@gmail.com>
2023-10-06 22:47:23 +0200

    💄 Add translate text field into translate screen

--- androidApp/src/main/kotlin/cz/jankotas/translator/android/translate/presentation/TranslateScreen.kt
+++ androidApp/src/main/kotlin/cz/jankotas/translator/android/translate/presentation/TranslateScreen.kt
@@ -1,5 +1,6 @@
 package cz.jankotas.translator.android.translate.presentation
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,18 +10,27 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
+import cz.jankotas.translator.android.R
 import cz.jankotas.translator.android.translate.presentation.components.LanguageDropDown
 import cz.jankotas.translator.android.translate.presentation.components.SwapLanguagesButton
+import cz.jankotas.translator.android.translate.presentation.components.TranslateTextField
 import cz.jankotas.translator.translate.presentation.TranslateEvent
 import cz.jankotas.translator.translate.presentation.TranslateState
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun TranslateScreen(
     state: TranslateState,
     onEvent: (TranslateEvent) -> Unit,
 ) {
+    val context = LocalContext.current
     Scaffold { padding ->
         LazyColumn(
             modifier = Modifier
@@ -53,6 +63,46 @@ fun TranslateScreen(
                         onSelectLanguage = { onEvent(TranslateEvent.ChooseToLanguage(it)) },
                     )
                 }
+            }
+            item {
+                val clipboardManager = LocalClipboardManager.current
+                val keyboardController = LocalSoftwareKeyboardController.current
+                TranslateTextField(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    fromText = state.fromText,
+                    toText = state.toText,
+                    isTranslating = state.isTranslating,
+                    fromLanguage = state.fromLanguage,
+                    toLanguage = state.toLanguage,
+                    onTranslateClick = {
+                        keyboardController?.hide()
+                        onEvent(TranslateEvent.Translate)
+                    },
+                    onTextChange = {
+                        onEvent(TranslateEvent.ChangeTranslationText(it))
+                    },
+                    onCopyClick = { text ->
+                        clipboardManager.setText(
+                            buildAnnotatedString {
+                                append(text = text)
+                            },
+                        )
+                        Toast.makeText(
+                            context,
+                            context.getString(R.string.copied_to_clipboard),
+                            Toast.LENGTH_LONG,
+                        ).show()
+                    },
+                    onCloseClick = {
+                        onEvent(TranslateEvent.CloseTranslation)
+                    },
+                    onSpeakerClick = {
+                    },
+                    onTextFieldClick = {
+                        onEvent(TranslateEvent.EditTranslation)
+                    },
+                )
             }
         }
     }
