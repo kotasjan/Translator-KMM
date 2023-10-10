c1b1f37cc97b625c91faf09b4fd6b37b0d84b732
Jan Kotas <kotasjn@gmail.com>
2023-10-10 15:06:39 +0200

    💄 Add progress button and gradient surface

---
+++ iosApp/iosApp/translate/presentation/components/LanguageDisplay.swift
@@ -0,0 +1,27 @@
+//
+//  LanguageDisplay.swift
+//  iosApp
+//
+//  Created by Jan Kotas on 10.10.2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+import shared
+
+struct LanguageDisplay: View {
+    var language: UiLanguage
+    
+    var body: some View {
+        HStack {
+            SmallLanguageIcon(language: language)
+                .padding(.trailing, 5)
+            Text(language.language.langName)
+                .foregroundColor(.lightBlue)
+        }
+    }
+}
+
+#Preview {
+    LanguageDisplay(language: UiLanguage(language: Language.czech, imageName: "czech"))
+}
---
+++ iosApp/iosApp/translate/presentation/components/ProgressButton.swift
@@ -0,0 +1,52 @@
+//
+//  ProgressButton.swift
+//  iosApp
+//
+//  Created by Jan Kotas on 10.10.2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+import shared
+
+struct ProgressButton: View {
+    var text: String
+    var isLoading: Bool
+    var onClick: () -> Void
+    
+    var body: some View {
+        Button(
+            action: {
+                if isLoading {
+                    onClick()
+                }
+            }
+        ) {
+            if isLoading {
+                ProgressView()
+                    .animation(.easeInOut, value: isLoading)
+                    .padding(5)
+                    .background(Color.primaryColor)
+                    .cornerRadius(100)
+                    .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            } else {
+                Text(text.uppercased())
+                    .animation(.easeInOut, value: isLoading)
+                    .padding(.horizontal)
+                    .padding(.vertical, 5)
+                    .font(.body.weight(.bold))
+                    .background(Color.primaryColor)
+                    .foregroundColor(Color.onPrimary)
+                    .cornerRadius(100)
+            }
+        }
+    }
+}
+
+#Preview {
+    ProgressButton(
+        text: "Hello",
+        isLoading: false,
+        onClick: {}
+    )
+}
