2f86f8a341521ab080122bd98a476fe13e23f5ec
Jan Kotas <kotasjn@gmail.com>
2023-10-19 15:49:16 +0200

    🐛 Fix @InstallIn hilt DI

--- androidApp/src/main/kotlin/cz/jankotas/voiceToText/di/VoiceToTextModule.kt
+++ androidApp/src/main/kotlin/cz/jankotas/voiceToText/di/VoiceToTextModule.kt
@@ -1,16 +1,16 @@
 package cz.jankotas.voiceToText.di
 
 import android.app.Application
-import androidx.lifecycle.ViewModel
 import cz.jankotas.translator.android.voiceToText.data.AndroidVoiceToTextParser
 import cz.jankotas.translator.voiceToText.domain.VoiceToTextParser
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
 
 @Module
-@InstallIn(ViewModel::class)
+@InstallIn(ViewModelComponent::class)
 object VoiceToTextModule {
     @Provides
     @ViewModelScoped
